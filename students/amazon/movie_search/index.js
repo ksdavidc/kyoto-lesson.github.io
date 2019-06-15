553344efb7b063f2bbb9b61f1a6d0accf0af08b9
kyoto-lesson <hello@kyotolesson.com>
2019-06-15 08:09:52 +0800

    movie exercise

---
+++ students/amazon/movie_search/index.js
@@ -0,0 +1,26 @@
+            
+const searchMovie = (event) => {
+  fetch(`http://www.omdbapi.com/?s=${event.currentTarget.value}&apikey=adf1f2d7`)
+    .then(response => response.json())
+    .then((data) => {
+      //console.log(data);
+      list.innerHTML = "";
+      let index = 0;
+      let item = "";
+      for (index=0; index<10;  index++){
+        if(data.Search[index].Poster!="N/A"){
+          item = `<div class="col-xs-4"><div class="shadow p-4 m-3 text-center"><img src='${data.Search[index].Poster}' alt='movie'><p class="m-0 mt-3 text-center">${data.Search[index].Title}</p></div></div>`;
+        }
+        else{
+          item = `<div class="col-xs-4"><div class="shadow p-4 m-3 text-center"><img src='images/noimage.jpg' alt='movie'><p class="m-0 mt-3 text-center">${data.Search[index].Title}</p></div></div>`;
+        }
+        list.insertAdjacentHTML('beforeend', item);
+      }
+    });
+    event.preventDefault();
+};
+
+const list = document.querySelector(".row");
+const input = document.querySelector("form input");
+input.addEventListener("keyup", searchMovie);
+
