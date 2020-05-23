1893495bd19780cab2ea4cd8502276aea630835a
alan <alanbogossian@gmail.com>
2020-05-23 13:53:02 +0800

    add missions and homeworks

---
+++ web_app/missions/assets/pages/shared/js/app.js
@@ -0,0 +1,13 @@
+function showImages(){
+    let images = document.getElementsByTagName("img");
+    for(let i = 0; i<images.length; i++){
+        images[i].classList.remove("d-none");
+    }
+}
+
+function hideImages(){
+    let images = document.getElementsByTagName("img");
+    for(let i = 0; i<images.length; i++){
+        images[i].classList.add("d-none");
+    }
+}
